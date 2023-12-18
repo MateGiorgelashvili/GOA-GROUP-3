0fe8c2cb1a4ffb8d0e5293ad5bb25af650a043cd
MateGiorgelashvili <cyanz0901@gmail.com>
2023-12-18 23:25:50 +0400

    other commit

---
+++ Day 44/Homework/HW 1/script.js
@@ -0,0 +1,10 @@
+function mySum(){
+  let num1 = Number(prompt("Enter Number 1: "))
+  let num2 = Number(prompt("Enter Number 2: "))
+  let num3 = Number(prompt("Enter Number 3: "))
+  console.log(num1 + num2 + num3)
+}
+mySum();
+
+
+
