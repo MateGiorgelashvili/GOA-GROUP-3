3672dc428251796e6de4e1ec16a51048a73c4286
MateGiorgelashvili <146206302+MateGiorgelashvili@users.noreply.github.com>
2024-01-29 22:57:13 +0400

    sum commit

--- Day 56/classwork/script.js
+++ Day 56/classwork/script.js
@@ -32,7 +32,24 @@
 //   console.log("chad welcome");
 // }
 
-for(let i = 1 ; i <= 100000; i++){
-   console.log(i + ". I love goa");
-}
+// for(let i = 1 ; i <= 100000; i++){
+//    console.log(i + ". I love goa");
+// }
 
+
+// let userInput = Number(prompt("Enter Number"))
+// if(userInput >= 0){
+//   console.log(userInput * -1);
+// }else if (userInput < 0){
+//   console.log(userInput * -1)
+// }
+
+let myArr = [1, 2, 3, 4, 5];
+let sum = 0;
+
+for (let i = 0; i < myArr.length; i++) {
+    sum += myArr[i];
+}
+let average = sum / myArr.length;
+
+console.log("average:", average);
