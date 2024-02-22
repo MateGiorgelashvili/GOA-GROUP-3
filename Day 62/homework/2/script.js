e25867926bf47376f56c7c009f149a0be9e0feb9
MateGiorgelashvili <cyanz0901@gmail.com>
2024-02-23 01:53:16 +0400

    commit

---
+++ Day 62/homework/2/script.js
@@ -0,0 +1,15 @@
+let twinOne = {
+  name: "danieli",
+  lastName: "kamechidze",
+  age: 10
+}
+
+let twinTwo = {
+  name: "danieli",
+  lastName: "kamechidze",
+  age: 10
+}
+
+console.log(twinOne.name === twinTwo.name);
+console.log(twinOne.lastName === twinTwo.lastName);
+console.log(twinOne.age === twinTwo.age);
